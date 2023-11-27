034a5ed708bae35d164098a011486f0ce646d247
Borja Montseny <68342939+borjaMontseny@users.noreply.github.com>
2023-11-27 19:09:33 +0100

    Commit amb correccions
    Arreglat les RegExps de Telèfons, ara comencen amb 9,8,7,6.

--- evaluate.js
+++ evaluate.js
@@ -6,45 +6,45 @@ function comprobarRegExp(regex, texto, checkEx) {
   }
 }
 
-window.onload = function() {
-    var regExpData = /^([0-2][0-9]|(3)[0-1])(\-)(((0)[0-9])|((1)[0-2]))(\-)\d{4}$/; // DD-MM-AAAA
-    var regExpData2 = /^([0-2][0-9]|(3)[0-1])(-|\/)(((0)[0-9])|((1)[0-2]))(-|\/)\d{4}$/; // DD-/MM-/AAAA
-    var regeExpTelefon = /^(?:\d{9}|\d{3} \d{3} \d{3}|\d{2} \d{3} \d{2} \d{2})$/;  // 977751487, 977 751 487, 97 712 34 56
-    var regeExpTelefon2 = /^(\+\d{3} ?)?(?:\d{9}|\d{3} \d{3} \d{3}|\d{2} \d{3} \d{2} \d{2})$/;
-    var regexpContrassenya = /^[0-9a-zàáèéíïòóúüçñ]{9,}$/i;
+window.onload = function () {
+  var regExpData = /^([0-2][0-9]|(3)[0-1])(\-)(((0)[0-9])|((1)[0-2]))(\-)\d{4}$/; // DD-MM-AAAA
+  var regExpData2 = /^([0-2][0-9]|(3)[0-1])(-|\/)(((0)[0-9])|((1)[0-2]))(-|\/)\d{4}$/;  // DD-/MM-/AAAA
+  var regeExpTelefon = /^(?:[9876]\d{8}|[9876]\d{2} \d{3} \d{3}|[9876]\d{1} \d{3} \d{2} \d{2})$/;  // 977751487, 977 751 487, 97 712 34 56
+  var regeExpTelefon2 = /^(\+\d{3} ?)?(?:[9876]\d{8}|[9876]\d{2} \d{3} \d{3}|[9876]\d{1} \d{3} \d{2} \d{2})$/;
+  var regexpContrassenya = /^[0-9a-zàáèéíïòóúüçñ]{9,}$/i;
 
-    var inputEx1 = document.getElementById("inputEx1");
-    var checkEx1 = document.getElementById("checkEx1");
+  var inputEx1 = document.getElementById("inputEx1");
+  var checkEx1 = document.getElementById("checkEx1");
 
-    var inputEx2 = document.getElementById("inputEx2");
-    var checkEx2 = document.getElementById("checkEx2");
+  var inputEx2 = document.getElementById("inputEx2");
+  var checkEx2 = document.getElementById("checkEx2");
 
-    var inputEx3 = document.getElementById("inputEx3");
-    var checkEx3 = document.getElementById("checkEx3");
+  var inputEx3 = document.getElementById("inputEx3");
+  var checkEx3 = document.getElementById("checkEx3");
 
-    var inputEx4 = document.getElementById("inputEx4");
-    var checkEx4 = document.getElementById("checkEx4");
+  var inputEx4 = document.getElementById("inputEx4");
+  var checkEx4 = document.getElementById("checkEx4");
 
-    var inputEx5 = document.getElementById("inputEx5");
-    var checkEx5 = document.getElementById("checkEx5");
+  var inputEx5 = document.getElementById("inputEx5");
+  var checkEx5 = document.getElementById("checkEx5");
 
-    inputEx1.addEventListener("input", function () {
-      comprobarRegExp(regExpData, inputEx1.value, checkEx1);
-    });
+  inputEx1.addEventListener("input", function () {
+    comprobarRegExp(regExpData, inputEx1.value, checkEx1);
+  });
 
-    inputEx2.addEventListener("input", function () {
-      comprobarRegExp(regExpData2, inputEx2.value, checkEx2);
-    });
+  inputEx2.addEventListener("input", function () {
+    comprobarRegExp(regExpData2, inputEx2.value, checkEx2);
+  });
 
-    inputEx3.addEventListener("input", function () {
-      comprobarRegExp(regeExpTelefon, inputEx3.value, checkEx3);
-    });
+  inputEx3.addEventListener("input", function () {
+    comprobarRegExp(regeExpTelefon, inputEx3.value, checkEx3);
+  });
 
-    inputEx4.addEventListener("input", function () {
-      comprobarRegExp(regeExpTelefon2, inputEx4.value, checkEx4);
-    });
+  inputEx4.addEventListener("input", function () {
+    comprobarRegExp(regeExpTelefon2, inputEx4.value, checkEx4);
+  });
 
-    inputEx5.addEventListener("input", function () {
-      comprobarRegExp(regexpContrassenya, inputEx5.value, checkEx5);
-    });
+  inputEx5.addEventListener("input", function () {
+    comprobarRegExp(regexpContrassenya, inputEx5.value, checkEx5);
+  });
 }
